348e30177523314a65ce29d59ffc51dbae8d2ccc
yunhwa2 <y0_0nxx@naver.com>
2023-12-06 14:50:35 +0900

    윤화 - 헤더 오른쪽 상단 수정

--- mn/src/main/java/com/mn/config/SecurityConfig.java
+++ mn/src/main/java/com/mn/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
                 //모든 사용자가 인증 없이 접근 가능
                 .mvcMatchers("/css/**","/js/**","/img/**").permitAll()
                 .mvcMatchers("/static/**").permitAll()
-                .mvcMatchers("/","/members/**","/item/**","/images/**","/Jiseong/**","/notice/**").permitAll()
+                .mvcMatchers("/","/**","/members/**","/item/**","/images/**","/Jiseong/**","/notice/**").permitAll()
                 //admin으로 시작하는 경로는 admin만 가능
                 .mvcMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated();
