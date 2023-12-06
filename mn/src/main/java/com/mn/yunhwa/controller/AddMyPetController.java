36969835b386dc772a6ffc732d50e40993979879
yunhwa2 <y0_0nxx@naver.com>
2023-12-06 17:42:48 +0900

    윤화 - 인덱스, 헤더, 푸터, 멍냥실종, 육아수첩, 마이페이지, 검색에러창, 동물등록 맵핑 및 프엔 작업 마무리함

--- mn/src/main/java/com/mn/yunhwa/controller/AddMyPetController.java
+++ mn/src/main/java/com/mn/yunhwa/controller/AddMyPetController.java
@@ -2,8 +2,13 @@ package com.mn.yunhwa.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class AddMyPetController {
+    @GetMapping("/pet/register")
+    public String petRegister(){
+        return "yunhwa/addMyPet";
+    }
 }
