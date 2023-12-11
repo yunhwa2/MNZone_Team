789d5a703ecfbf09123d7873cc6ec49f74348575
PeterJiseong <wltjd9407@naver.com>
2023-12-11 17:50:07 +0900

    지성 - 공지사항 완료 (디자인 제외), 이전 다음 버튼, 목록보기 버튼 기능 구현, 병원 시작

--- mn/src/main/java/com/mn/entity/Hospital.java
+++ mn/src/main/java/com/mn/entity/Hospital.java
@@ -1,4 +1,10 @@
 package com.mn.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "hospital")
 public class Hospital {
+    private Long hospitalName;
 }
