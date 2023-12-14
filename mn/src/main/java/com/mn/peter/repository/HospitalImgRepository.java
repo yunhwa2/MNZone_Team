1566ddfe62e55664f1260c8f04f42f7ef77b5a5e
PeterJiseong <wltjd9407@naver.com>
2023-12-14 16:41:28 +0900

    지성 - 복구작업 revert 후 서버 실행 오류 수정

--- mn/src/main/java/com/mn/peter/repository/HospitalImgRepository.java
+++ mn/src/main/java/com/mn/peter/repository/HospitalImgRepository.java
@@ -3,5 +3,5 @@ package com.mn.peter.repository;
 import com.mn.entity.HospitalImg;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HospitalImgRepository extends JpaRepository<Long, HospitalImg> {
+public interface HospitalImgRepository extends JpaRepository<HospitalImg, Long> {
 }
